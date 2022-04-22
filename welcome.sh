3432c6f4cc033569af807597c967c3259ebd6abc
4n0nym0u5 <dimensionc132@riseup.net>
2022-04-22 17:54:34 +0000

    Add files via upload

---
+++ welcome.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# this is an introduction screen to Dead Switch and ensure that people understand what it is
+ 
+resize -s 35 80
+clear
+echo "$(tput setaf 1)"
+echo "██████╗ ███████╗ █████╗ ██████╗ ███████╗██╗    ██╗██╗████████╗ ██████╗██╗  ██╗"
+echo "██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔════╝██║    ██║██║╚══██╔══╝██╔════╝██║  ██║"
+echo "██║  ██║█████╗  ███████║██║  ██║███████╗██║ █╗ ██║██║   ██║   ██║     ███████║"
+echo "██║  ██║██╔══╝  ██╔══██║██║  ██║╚════██║██║███╗██║██║   ██║   ██║     ██╔══██║"
+echo "██████╔╝███████╗██║  ██║██████╔╝███████║╚███╔███╔╝██║   ██║   ╚██████╗██║  ██║"
+echo "╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝ ╚══╝╚══╝ ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝"
+echo ""
+echo "$(tput setaf 3)A dead man switch is a mechanism (digital or physical) that triggers when the user fails to perform an action. In the scope of this function, Dead Switch is a security application designed protect your data/secrets in case you are threatened by violence from people, organizations or government agencies. It is a safeguard for your data/secrets, that if such a scenario were to occur, an email will be sent with your data/secrets to a specified person for safekeeping." | fold -sw 80
+echo ""
+echo "The most famous use of a Dead Man Switch in recent times was by Edward Snowden who distributed encrypted copies of his secret documents to various trusted people, and then setup a Dead Man Switch to email the encryption key." | fold -sw 80
+echo ""
+echo "Dead Switch will help you create a message as well as an optional attachment that will be sent to a specified email address. If you fail to check-in after a specified interval, Dead Switch assumes that you no longer haveto access this computer or have been 'incapacited' and will then send the email with optional attachment." | fold -sw 80
+echo ""
+echo "$(tput setaf 1)Do you wish to continue and run setup?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) bash setup.sh; break;;
+        No ) echo "Exiting Dead Switch program. Goodbye"; exit;;
+        *)
+        echo "Invalid entry."
+        break
+        ;;
+esac
+done
+
